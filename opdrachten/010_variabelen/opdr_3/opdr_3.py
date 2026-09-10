746996e6cb0909b5d57aedd4bfe478a340063cef
Ormon4224 <97054517@st.deltion.nl>
2026-09-10 15:48:20 +0200

    Opdracht 1

--- opdrachten/010_variabelen/opdr_3/opdr_3.py
+++ opdrachten/010_variabelen/opdr_3/opdr_3.py
@@ -4,6 +4,6 @@
 
 # Hier komt je code...
 
-my_dict = ...
+my_dict = {"naam" : "William", "Achternaam" : "Van der broek", "Leeftijd" : str(23), "Favoriete kleur" : "blauw"}
 
-print()
+print(my_dict)
